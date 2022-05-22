20bfd97bd6e35326161aec35f7064a73af852702
Mariano Girbau <marianogirbau@gmail.com>
2022-05-22 18:36:06 -0300

    Ejercicio 1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,24 @@
 ﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Probamos con otro push");
+/* Console.WriteLine("Ingrese texto");
+string numero1 = Console.ReadLine();
+int numero = 4;
+Console.WriteLine("Uds escribio " + texto + " y el numero " + numero); */
+
+//double resultado = Convert.ToDouble(numero1) + Convert.ToDouble(numero2); //Int32.Parse
+
+/*Ejercicio 1*/
+
+Console.WriteLine("Ingrese un numero:");
+string numero = Console.ReadLine();
+int N = Convert.ToInt32(numero);
+int Ninv=0;
+if (N>=0) //Si es 0 o negativo evitar la inversion
+{
+    while (N>0)
+    {
+        Ninv = Ninv * 10 + (N % 10);
+        N /= 10;
+    }
+}
+Console.WriteLine("El numero es: " + numero);
+Console.WriteLine("El numero invertido es: " + Ninv);
