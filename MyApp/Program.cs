6d0eef870e9186424fcad006d62960d11f59bd9d
Mariano Girbau <marianogirbau@gmail.com>
2022-05-23 21:12:49 -0300

    CalculadoraV2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Uds escribio " + texto + " y el numero " + numero); */
 
 /*Ejercicio 1*/
 
-Console.WriteLine("Ingrese un numero:");
+/*Console.WriteLine("Ingrese un numero:");
 string numero = Console.ReadLine();
 int N = Convert.ToInt32(numero);
 int Ninv=0;
@@ -21,4 +21,60 @@ if (N>=0) //Si es 0 o negativo evitar la inversion
     }
 }
 Console.WriteLine("El numero es: " + numero);
-Console.WriteLine("El numero invertido es: " + Ninv);
+Console.WriteLine("El numero invertido es: " + Ninv);*/
+
+/***************************************Calculadora*************************************************/
+
+
+string numero1,menu;
+double N1=0;
+double resultado=0;
+do
+{
+    Console.WriteLine("¿Que operacion desea realizar?");
+    Console.WriteLine("1.Valor absoluto\n2.Cuadrado\n3.Raiz cuadrada\n4.Seno\n5.Coseno\n6.Parte entera");
+    int flag = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese un numero:");
+    numero1 = Console.ReadLine();
+    N1 = Convert.ToDouble(numero1);
+    switch (flag)
+    {
+        case 1:
+            resultado=Math.Abs(N1);
+        break;
+
+        case 2:
+            resultado=Math.Pow(N1,2);
+        break;
+
+        case 3:
+            resultado=Math.Sqrt(N1);
+        break;
+
+        case 4:
+            resultado=Math.Sin(N1); 
+        break;
+        
+        case 5:
+            resultado=Math.Cos(N1);
+        break;
+
+        case 6:
+            resultado=Math.Truncate(N1); //Calcula la parte entera de un número de punto flotante de precisión doble especificado.
+        break;
+
+        default:
+            Console.WriteLine("Error");
+        break;
+    }
+    Console.WriteLine("El resultado es: " + resultado);
+    Console.WriteLine("¿Desea realizar otra opreacion? 1.Si 2.No");
+    menu = Console.ReadLine();
+} while (menu=="1");
+
+Console.WriteLine("Ingrese dos numeros para comparar: ");
+int NComp1 = Convert.ToInt32(Console.ReadLine());
+int NComp2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("El mayor de los numeros es: " + Math.Max(NComp1,NComp2));
+Console.WriteLine("El mayor de los numeros es: " + Math.Min(NComp1,NComp2));
